8d5f7b39add238b8bd7a93a057e809c37ec47ec6
Marcus Schäfer <marcus.schaefer@gmail.com>
2021-12-13 12:36:37 +0100

    Added kernel example

---
+++ projects/kernel/kernel-kvmsmall/config.sh
@@ -0,0 +1,15 @@
+# The version of the main tarball to use
+SRCVERSION=5.3
+# variant of the kernel-source package, either empty or "-rt"
+VARIANT=
+# Use new style livepatch package names
+LIVEPATCH=livepatch
+# Compile binary devicetrees on master and stable branches.
+BUILD_DTBS="Yes"
+# buildservice projects to build the kernel against
+OBS_PROJECT=openSUSE:Leap:15.2:Update
+# Bugzilla info
+BUGZILLA_SERVER="apibugzilla.suse.com"
+BUGZILLA_PRODUCT="openSUSE Distribution"
+# Check the sorted patches section of series.conf
+SORT_SERIES=yes
